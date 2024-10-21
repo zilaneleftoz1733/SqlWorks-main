b47bb876e092f524bf6b6382b6458043eaa33edb
zilanelefoz <lzilaneleftozl@gmail.com>
2024-10-21 14:40:28 +0300

    prodecure yazildi

--- RaporTablosuOlusturma.sql
+++ RaporTablosuOlusturma.sql
@@ -1,4 +1,4 @@
-create procedure RaporTablosuHazirla (@tableName nvarchar(50))
+alter procedure RaporTablosuHazirla (@tableName nvarchar(50))
 as 
 Begin 
 
@@ -31,19 +31,19 @@ exec (@sql) -- Degiskenleri exec ederken mutlaka parantez icerisinde yazilmasi g
 declare @phone varchar(20)
 
 declare cursor_ivrPhone CURSOR for  -- Cursor Tanimlamasi 
-select PhoneNumber from IvrPhone
+select PhoneNumber from IvrPhoneNumber
 
 open cursor_ivrPhone
 FETCH next from cursor_ivrPhone into @phone
 while @@FETCH_STATUS=0
 Begin 
 
-set @sql = 'insert into ##' +@tableName+ '(Phone) values ('+@phone+')'
+set @sql = 'insert into ##' +@tableName+ ' (Phone) values ('''+@phone+''')' -- tek tirnak olusumuna dikkat
+print @sql 
 exec (@sql)
 FETCH next from cursor_ivrPhone into @phone
 End
 CLOSE cursor_ivrPhone
 DEALLOCATE cursor_ivrPhone
---drop table ##Rapor2
+
 End
---select * from ##Rapor2
